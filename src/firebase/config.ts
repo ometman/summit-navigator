dc6ad292b205afc9d383587296d490185819f3eb
Stack Varsity <stackvarsity@gmail.com>
2025-10-29 10:05:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5604296706-34a23",
+  "appId": "1:991800606229:web:424fddd3d691df9319981c",
+  "apiKey": "AIzaSyAhBYBtRUvJZuT0wO0XM9Bk9MKe0bE4gn8",
+  "authDomain": "studio-5604296706-34a23.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "991800606229"
+};
